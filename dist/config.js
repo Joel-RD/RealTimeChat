1d9b39cd587235c46055b278033f05b726ee58e0
JoelRD:01 <eudy.joel.reyes@gmail.com>
2025-05-09 11:41:37 -0400

    Remove unused DB_URL variables and update start script in package.json

--- dist/config.js
+++ dist/config.js
@@ -1,4 +1,4 @@
-const { PORT_SERVER, URL_LOCAL_HOST, DB_URL_LOCAL, DB_URL_CLOUD, NODE_ENV } = process.env;
+const { PORT_SERVER, URL_LOCAL_HOST, NODE_ENV } = process.env;
 export const config = {
     PORT_SERVER: PORT_SERVER || 3000,
     URL_LOCAL: URL_LOCAL_HOST,
