aa12fb7dfa213831ece9ec5cfcc2b3af9a3e06e7
JoelRD:01 <eudy.joel.reyes@gmail.com>
2025-05-09 11:01:53 -0400

    Remove DB_URL from config and environment variables

--- config.ts
+++ config.ts
@@ -1,17 +1,15 @@
 interface Config {
   PORT_SERVER: number | string,
   URL_LOCAL: string,
-  DB_URL: string,
   console_log: (...args: unknown[]) => void
 }
 
-const { PORT_SERVER, URL_LOCAL_HOST, DB_URL_LOCAL, DB_URL_CLOUD, NODE_ENV } =
+const { PORT_SERVER, URL_LOCAL_HOST, NODE_ENV } =
   process.env;
 
 export const config: Config = {
   PORT_SERVER: PORT_SERVER || 3000,
   URL_LOCAL: URL_LOCAL_HOST,
-  DB_URL: NODE_ENV === "production" ? DB_URL_CLOUD : DB_URL_LOCAL,
   console_log: (...args) => {
     if (NODE_ENV !== "production") {
       console.log(...args); // Solo loguea en entornos que no sean producción
--- dist/config.js
+++ dist/config.js
@@ -2,7 +2,6 @@ const { PORT_SERVER, URL_LOCAL_HOST, DB_URL_LOCAL, DB_URL_CLOUD, NODE_ENV } = pr
 export const config = {
     PORT_SERVER: PORT_SERVER || 3000,
     URL_LOCAL: URL_LOCAL_HOST,
-    DB_URL: NODE_ENV === "production" ? DB_URL_CLOUD : DB_URL_LOCAL,
     console_log: (...args) => {
         if (NODE_ENV !== "production") {
             console.log(...args); // Solo loguea en entornos que no sean producción
